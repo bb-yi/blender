7ad759da8934e5a2fc2189fd4b2ff20f43700835
bbyi <2181292830@qq.com>
2026-08-01 02:19:31 +0800

    fix(eevee): conditionally bind volume light shader resources

--- source/blender/draw/engines/eevee/shaders/eevee_light_eval.bsl.hh
+++ source/blender/draw/engines/eevee/shaders/eevee_light_eval.bsl.hh
@@ -38,12 +38,13 @@
 namespace eevee {
 
 struct LightEvalData {
-  [[resource_table]] srt_t<ShadowRenderData> shadow_data;
-  [[resource_table]] srt_t<UtilityTexture> utility_tx;
-  [[resource_table]] srt_t<LightShaderEvalData> light_shader_data;
-
   [[compilation_constant]] bool use_light_shader_texture_eval;
   [[compilation_constant]] bool use_light_shader_surfel_eval;
+
+  [[resource_table]] srt_t<ShadowRenderData> shadow_data;
+  [[resource_table]] srt_t<UtilityTexture> utility_tx;
+  [[resource_table, condition(use_light_shader_texture_eval)]] srt_t<LightShaderEvalData>
+      light_shader_data;
   [[resource_table, condition(use_light_shader_surfel_eval)]] srt_t<LightShaderSurfelEvalData>
       light_shader_surfel_data;
   [[compilation_constant]] int light_closure_eval_count_reflect;
